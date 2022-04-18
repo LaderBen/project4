ea3a84fa5f5fa37fcafd3e3b82c129a70ea07635
laderben <z874154748@sina.com>
2022-04-17 22:22:33 -0400

    add simple_pages auth and context_processors

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,20 @@
 """A simple flask web app"""
 import os
 from flask import Flask
+from flask import Blueprint, render_template, abort
+from flask_wtf.csrf import CSRFProtect
+
+import flask_login
+from flask_bootstrap import Bootstrap5
+
 from app.cli import create_database
 from app.db import db
+from app.simple_pages import simple_pages
+from app.auth import auth
 from app.db.models import User
+from app.context_processors import utility_text_processors
 
+login_manager = flask_login.LoginManager()
 
 def page_not_found(e):
     return render_template("404.html"), 404
@@ -14,7 +24,19 @@ def create_app():
     app = Flask(__name__)
     app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
 
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
+
+    csrf = CSRFProtect(app)
+    bootstrap = Bootstrap5(app)
+
     app.register_error_handler(404, page_not_found)
+
+    app.register_blueprint(simple_pages)
+    app.register_blueprint(auth)
+
+    app.context_processor(utility_text_processors)
+
     db_dir = "database/db.sqlite"
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -22,8 +44,15 @@ def create_app():
     # add command function to cli commands
     app.cli.add_command(create_database)
 
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
+    # @app.route('/')
+    # def hello():
+    #     return 'Hello, World!'
 
     return app
+
+@login_manager.user_loader
+def user_loader(user_id):
+    try:
+        return User.query.get(int(user_id))
+    except:
+        return None

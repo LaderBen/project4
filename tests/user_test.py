886cee7c3738b5a5c52d25f956206f34a118f73e
laderben <z874154748@sina.com>
2022-05-04 12:46:49 -0400

    fixed a bug when user upload new csv file it only show the recode that just uploaded

--- tests/user_test.py
+++ tests/user_test.py
@@ -4,15 +4,17 @@ from pathlib import Path
 
 from app.db.models import User
 
+
 def test_user_register(client):
     data = {
-        'email' : "test@test.com",
-        'password' : "testtest",
+        'email': "test@test.com",
+        'password': "testtest",
         'confirm': "testtest"
     }
     client.post('/register', data=data)
     user = User.query.filter_by(email='test@test.com').first()
     assert user.email == 'test@test.com'
+
 
 def test_user_login(client):
     data = {
@@ -22,12 +24,13 @@ def test_user_login(client):
     }
     client.post('/register', data=data)
     data = {
-        'email' : "test@test.com",
-        'password' : "testtest",
+        'email': "test@test.com",
+        'password': "testtest",
     }
     client.post('/login', data=data)
     response = client.get('/dashboard')
     assert response.status_code == 200
+
 
 def test_user_file_upload(client):
     data = {
@@ -37,13 +40,13 @@ def test_user_file_upload(client):
     }
     client.post('/register', data=data)
     data = {
-        'email' : "test@test.com",
-        'password' : "testtest",
+        'email': "test@test.com",
+        'password': "testtest",
     }
     client.post('/login', data=data)
     root = Path(__file__).parent.parent
-    test_file = root/'tests'/'test_transactions.csv'
-    upload_dir = root/'app'/'uploads'
+    test_file = root / 'tests' / 'test_transactions.csv'
+    upload_dir = root / 'app' / 'uploads'
     if not os.path.exists(upload_dir):
         os.mkdir(upload_dir)
     if len(os.listdir(upload_dir)) != 0:
@@ -51,12 +54,13 @@ def test_user_file_upload(client):
             os.remove(str(upload_dir) + '/' + file)
     assert len(os.listdir(upload_dir)) == 0
     data = {
-        'file': open(test_file,'rb')
+        'file': open(test_file, 'rb')
     }
     client.post('/transactions/upload', data=data)
     assert len(os.listdir(upload_dir)) == 1
     for file in os.listdir(upload_dir):
-        os.remove(str(upload_dir) + '/'+file)
+        os.remove(str(upload_dir) + '/' + file)
+
 
 def test_user_to_transaction_relationship(client):
     data = {
@@ -66,13 +70,13 @@ def test_user_to_transaction_relationship(client):
     }
     client.post('/register', data=data)
     data = {
-        'email' : "test@test.com",
-        'password' : "testtest",
+        'email': "test@test.com",
+        'password': "testtest",
     }
     client.post('/login', data=data)
     root = Path(__file__).parent.parent
-    test_file = root/'tests'/'test_transactions.csv'
-    upload_dir = root/'app'/'uploads'
+    test_file = root / 'tests' / 'test_transactions.csv'
+    upload_dir = root / 'app' / 'uploads'
     if not os.path.exists(upload_dir):
         os.mkdir(upload_dir)
     if len(os.listdir(upload_dir)) != 0:
@@ -80,12 +84,12 @@ def test_user_to_transaction_relationship(client):
             os.remove(str(upload_dir) + '/' + file)
     assert len(os.listdir(upload_dir)) == 0
     data = {
-        'file': open(test_file,'rb')
+        'file': open(test_file, 'rb')
     }
     client.post('/transactions/upload', data=data)
     assert len(os.listdir(upload_dir)) == 1
     for file in os.listdir(upload_dir):
-        os.remove(str(upload_dir) + '/'+file)
+        os.remove(str(upload_dir) + '/' + file)
     response = client.get('/transactions')
     assert b'DEBIT' in response.data
     # logout current user
@@ -98,9 +102,70 @@ def test_user_to_transaction_relationship(client):
     }
     client.post('/register', data=data)
     data = {
-        'email' : "test1@test.com",
-        'password' : "testtest",
+        'email': "test1@test.com",
+        'password': "testtest",
     }
     client.post('/login', data=data)
     response = client.get('/transactions')
     assert b'DEBIT' not in response.data
+
+
+def test_user_upload_wrong_file_type(client):
+    data = {
+        'email': "test@test.com",
+        'password': "testtest",
+        'confirm': "testtest"
+    }
+    client.post('/register', data=data)
+    data = {
+        'email': "test@test.com",
+        'password': "testtest",
+    }
+    client.post('/login', data=data)
+    root = Path(__file__).parent.parent
+    test_file = root / 'tests' / 'test.txt'
+    upload_dir = root / 'app' / 'uploads'
+    if not os.path.exists(upload_dir):
+        os.mkdir(upload_dir)
+    if len(os.listdir(upload_dir)) != 0:
+        for file in os.listdir(upload_dir):
+            os.remove(str(upload_dir) + '/' + file)
+    assert len(os.listdir(upload_dir)) == 0
+    data = {
+        'file': open(test_file, 'rb')
+    }
+    response = client.post('/transactions/upload', data=data)
+    assert b'csv file only' in response.data
+    for file in os.listdir(upload_dir):
+        os.remove(str(upload_dir) + '/' + file)
+
+
+def test_user_upload_wrong_file_content(client):
+    data = {
+        'email': "test@test.com",
+        'password': "testtest",
+        'confirm': "testtest"
+    }
+    client.post('/register', data=data)
+    data = {
+        'email': "test@test.com",
+        'password': "testtest",
+    }
+    client.post('/login', data=data)
+    root = Path(__file__).parent.parent
+    test_file = root / 'tests' / 'test_wrong_transactions.csv'
+    upload_dir = root / 'app' / 'uploads'
+    if not os.path.exists(upload_dir):
+        os.mkdir(upload_dir)
+    if len(os.listdir(upload_dir)) != 0:
+        for file in os.listdir(upload_dir):
+            os.remove(str(upload_dir) + '/' + file)
+    assert len(os.listdir(upload_dir)) == 0
+    data = {
+        'file': open(test_file, 'rb')
+    }
+    client.post('/transactions/upload', data=data)
+    response = client.get('/transactions/upload')
+    assert b'please check the content of your file' in response.data
+    for file in os.listdir(upload_dir):
+        os.remove(str(upload_dir) + '/' + file)
